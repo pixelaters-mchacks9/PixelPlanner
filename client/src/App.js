09cd6e6c0de2b09b84e4bc2e1d747ffac5950426
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2022-01-22 11:42:03 -0500

    Fix dataset

--- client/src/App.js
+++ client/src/App.js
@@ -21,81 +21,79 @@
 //     </div>
 //   );
 // }
-// import * as React from 'react';
-// import Paper from '@material-ui/core/Paper';
-// import { ViewState } from '@devexpress/dx-react-scheduler';
-// import {
-//   Scheduler,
-//   DayView,
-//   Appointments,
-// } from '@devexpress/dx-react-scheduler-material-ui';
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
 
-// const sampleData = require('./data/calendar-data.json')
-// const currentDate = '2018-11-01';
-// const schedulerData = [
-//   { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-//   { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-// ];
-// function App() {
-//   return (
-//     <Paper>
-//       <Scheduler
-//         data={sampleData}
-//       >
-//         <ViewState
-//           currentDate={currentDate}
-//         />
-//         <DayView
-//           startDayHour={9}
-//           endDayHour={14}
-//         />
-//         <Appointments />
-//       </Scheduler>
-//     </Paper>
-//   );
-// }
-// export default App;
-import React from 'react';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+const data = require('./data/calendar-data.json')
+const sampleData = data.SampleData
+const currentDate = '2022-01-22';
 
-import { styleReset, List, ListItem, Divider } from 'react95';
-// pick a theme of your choice
-import original from "react95/dist/themes/original";
-// original Windows95 font (optionally)
-import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
-import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
-
-const GlobalStyles = createGlobalStyle`
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
-    font-weight: 400;
-    font-style: normal
-  }
-  @font-face {
-    font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
-    font-weight: bold;
-    font-style: normal
-  }
-  body {
-    font-family: 'ms_sans_serif';
-  }
-  ${styleReset}
-`;
-
-const App = () => (
-  <div>
-    <GlobalStyles />
-    <ThemeProvider theme={original}>
-      <List>
-        <ListItem>🎤 Sing</ListItem>
-        <ListItem>💃🏻 Dance</ListItem>
-        <Divider />
-        <ListItem disabled>😴 Sleep</ListItem>
-      </List>
-    </ThemeProvider>
-  </div>
-);
-
+function App() {
+  return (
+    <Paper>
+      <Scheduler
+        data={sampleData}
+      >
+        <ViewState
+          currentDate={currentDate}
+        />
+        <DayView
+          startDayHour={9}
+          endDayHour={14}
+        />
+        <Appointments />
+      </Scheduler>
+    </Paper>
+  );
+}
 export default App;
+// import React from 'react';
+// import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+// import { styleReset, List, ListItem, Divider } from 'react95';
+// // pick a theme of your choice
+// import original from "react95/dist/themes/original";
+// // original Windows95 font (optionally)
+// import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
+// import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
+
+// const GlobalStyles = createGlobalStyle`
+//   @font-face {
+//     font-family: 'ms_sans_serif';
+//     src: url('${ms_sans_serif}') format('woff2');
+//     font-weight: 400;
+//     font-style: normal
+//   }
+//   @font-face {
+//     font-family: 'ms_sans_serif';
+//     src: url('${ms_sans_serif_bold}') format('woff2');
+//     font-weight: bold;
+//     font-style: normal
+//   }
+//   body {
+//     font-family: 'ms_sans_serif';
+//   }
+//   ${styleReset}
+// `;
+
+// const App = () => (
+//   <div>
+//     <GlobalStyles />
+//     <ThemeProvider theme={original}>
+//       <List>
+//         <ListItem>🎤 Sing</ListItem>
+//         <ListItem>💃🏻 Dance</ListItem>
+//         <Divider />
+//         <ListItem disabled>😴 Sleep</ListItem>
+//       </List>
+//     </ThemeProvider>
+//   </div>
+// );
+
+// export default App;
