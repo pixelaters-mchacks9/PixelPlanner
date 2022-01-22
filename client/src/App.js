e1589b82d7ec0eec30ceba8da167652b306e2088
AngelaSposato <angelasposato5@gmail.com>
2022-01-22 13:43:48 -0500

    Remove unnecessary imports in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,6 @@
 import * as React from 'react';
-import Paper from '@material-ui/core/Paper';
-import { ViewState } from '@devexpress/dx-react-scheduler';
-import {
-  Scheduler,
-  DayView,
-  Appointments,
-  AppointmentForm, 
-  AppointmentTooltip
-} from '@devexpress/dx-react-scheduler-material-ui';
-
 import Schedule from './components/Schedule';
-const currentDate = '2018-11-01';
-const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
+
 function App() {
   return (
     <Schedule></Schedule>
