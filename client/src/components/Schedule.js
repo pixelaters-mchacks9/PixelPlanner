8873ad882f86a18c57c43c1cd5057c921bd495f1
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2022-01-22 13:46:32 -0500

    Sync App and Schedule components

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -1,95 +1,16 @@
-/* import React, { Component } from "react";
-import Paper from "@material-ui/core/Paper";
-import { ViewState } from "@devexpress/dx-react-scheduler";
-import { EditingState } from "@devexpress/dx-react-scheduler";
-import { IntegratedEditing } from "@devexpress/dx-react-scheduler";
-import {
-  Scheduler,
-  DayView,
-  Appointments,
-  AppointmentForm,
-  AppointmentTooltip,
-  ConfirmationDialog,
-} from "@devexpress/dx-react-scheduler-material-ui";
-
-const currentDate = "2018-11-01";
-const schedulerData = [
-  {
-    startDate: "2018-11-01T09:45",
-    endDate: "2018-11-01T11:00",
-    title: "Meeting",
-  },
-  {
-    startDate: "2018-11-01T12:00",
-    endDate: "2018-11-01T13:30",
-    title: "Go to a gym",
-  },
-];
-
-class Schedule extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: appointments,
-      currentDate: "2018-06-27",
-    };
-
-    this.commitChanges = this.commitChanges.bind(this);
-  }
-
-  commitChanges({ added, changed, deleted }) {
-    this.setState((state) => {
-      let { data } = state;
-      if (added) {
-        const startingAddedId =
-          data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, ...added }];
-      }
-      if (changed) {
-        data = data.map((appointment) =>
-          changed[appointment.id]
-            ? { ...appointment, ...changed[appointment.id] }
-            : appointment
-        );
-      }
-      if (deleted !== undefined) {
-        data = data.filter((appointment) => appointment.id !== deleted);
-      }
-      return { data };
-    });
-  }
-
-  render() {
-    return (
-      <Paper>
-        <Scheduler data={data} height={660}>
-          <ViewState currentDate={currentDate} />
-          <EditingState onCommitChanges={this.commitChanges} />
-          <IntegratedEditing />
-          <DayView startDayHour={9} endDayHour={19} />
-          <ConfirmationDialog />
-          <Appointments />
-          <AppointmentTooltip showOpenButton showDeleteButton />
-          <AppointmentForm />
-        </Scheduler>
-      </Paper>
-    );
-  }
-}
-
-export default Schedule;
- */
-
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   DayView,
+  WeekView,
   Appointments,
   AppointmentForm,
   AppointmentTooltip,
   ConfirmationDialog,
+  ViewSwitcher, 
+  Toolbar
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 import { appointments } from '../appointments';
@@ -139,10 +60,14 @@ export default class Schedule extends React.PureComponent {
             onCommitChanges={this.commitChanges}
           />
           <IntegratedEditing />
-          <DayView
+          <WeekView
             startDayHour={9}
             endDayHour={19}
           />
+          <DayView 
+            startDayHour={9}
+            endDayHour={19}
+            />
           <ConfirmationDialog />
           <Appointments />
           <AppointmentTooltip
@@ -150,6 +75,8 @@ export default class Schedule extends React.PureComponent {
             showDeleteButton
           />
           <AppointmentForm />
+          <Toolbar />
+          <ViewSwitcher />
         </Scheduler>
       </Paper>
     );
