4161133c2b1eab4e81c80295665b980192a8e9c4
Emilia Krichevsky <emiliakrichevsky@gmail.com>
2022-01-22 22:39:17 -0500

    Remove unused imports

--- client/src/components/LoadingBar.js
+++ client/src/components/LoadingBar.js
@@ -1,8 +1,5 @@
 import { Component } from "react";
-import styled from 'styled-components';
-import { withStyles } from '@material-ui/core/styles';
 import { LoadingIndicator } from 'react95';
-import { Toolbar } from '@devexpress/dx-react-scheduler-material-ui';
 
 export default class LoadingBar extends Component {
   constructor(props) {
@@ -11,7 +8,7 @@ export default class LoadingBar extends Component {
   render() {
     return (
       <div style={{ padding: '1rem' }}>
-        <p style={{ textAlign: 'center', marginBottom: '0.5rem' }}>Loading...</p>
+        <p style={{ textAlign: 'center' }}>Loading...</p>
         <LoadingIndicator isLoading />
       </div>
     );
